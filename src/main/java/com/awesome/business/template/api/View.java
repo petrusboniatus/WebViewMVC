f45b1bbf91efba5edcead5c4dba15a6cd720327f
petrusboniatus <pedrobuenisimo@gmail.com>
2017-03-22 17:07:16 +0100

    Update View.java

--- src/main/java/com/awesome/business/template/api/View.java
+++ src/main/java/com/awesome/business/template/api/View.java
@@ -3,7 +3,7 @@ package com.awesome.business.template.api;
 import javafx.scene.layout.VBox;
 
 /**
- * Created by Carlos Couto Cerdeira on 3/22/17.
+ * Created by Carlos Couto Cerdeira and Pedro Bueno on 3/22/17.
  */
 public interface View {
 
